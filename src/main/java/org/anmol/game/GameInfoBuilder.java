98d83e6ee053592d1823c5f6dae2f9fe616662e9
Anmol Yadav <anmolyadav084@gmail.com>
2024-10-04 22:48:32 +0530

    Added Builder Design pattern for GameInfo.

---
+++ src/main/java/org/anmol/game/GameInfoBuilder.java
@@ -0,0 +1,38 @@
+package org.anmol.game;
+
+public class GameInfoBuilder {
+    private boolean hasFork;
+    private String winner;
+    private boolean isOver;
+    private Player player;
+    private int numberOfMoves;
+
+    public GameInfoBuilder isOver(boolean isOver) {
+        this.isOver = isOver;
+        return this;
+    }
+
+    public GameInfoBuilder winner(String winner) {
+        this.winner = winner;
+        return this;
+    }
+
+    public GameInfoBuilder hasFork(boolean hasFork) {
+        this.hasFork = hasFork;
+        return this;
+    }
+
+    public GameInfoBuilder player(Player player) {
+        this.player = player;
+        return this;
+    }
+
+    public GameInfoBuilder numberOfMoves(int numberOfMoves) {
+        this.numberOfMoves = numberOfMoves;
+        return this;
+    }
+
+    public GameInfo build() {
+        return new GameInfo(isOver, winner, hasFork, player, numberOfMoves);
+    }
+}
